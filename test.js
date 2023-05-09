b4947222c93eb2ad023abccdf6c17f232d8eede9
Alex Breedveld <alex@al3xdev.com>
2023-05-09 18:33:02 +0000

    Update CI/CD

--- test.js
+++
@@ -1,9 +0,0 @@
-const {exec} = require('child_process');
-const homedir = require('os').homedir();
-const { writeFileSync } = require('fs');
-const openvncscript = "#!/bin/bash\nif [ $(gp ports list | grep $1 | cut -d \"|\" -f 3 | grep \"open\" -c) == 1 ];then\n\texport GITPOD_URL_PROTOLESS=$(echo $GITPOD_WORKSPACE_URL | cut -d \"/\" -f 3)\n\texport VNC_URL=\"https://\"$1\"-\"$GITPOD_URL_PROTOLESS\"/vnc.html?autoconnect=true&reconnect=true&resize=remote\"\n\tif [ $2 == 0 ];then\n\t\tgp preview $VNC_URL\n\tfi\n\tif [ $2 == 1 ];then\n\t\tsensible-browser $VNC_URL\n\tfi\nelse\n\techo \"The VNC server is not running on port $1 or the Docker image wasn't initialized.\"\nfi";
-console.log(openvncscript);
-console.log(homedir);
-
-writeFileSync(`${homedir}/.local/bin/open-vnc`, openvncscript);
-exec(`chmod +x ${homedir}/.local/bin/open-vnc`);
